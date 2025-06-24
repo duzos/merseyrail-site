432f24b4ad28785c196e26fd9f57aa6d27cae5b1
James Hall <james.hall01@outlook.com>
2025-06-24 12:24:55 +0100

    feat: photos feat: departure sfx feat: 54 diagram

--- train.js
+++ train.js
@@ -1,9 +1,13 @@
 class Train {
-    constructor(cId, startService, endService, title = `Class ${cId}`, width = "100vw", height = "12vh", src = `img/train/${cId}.png`) {
+    constructor(cId, startService, endService, title = `Class ${cId}`, width = "100vw", height = "12vh", stationPosition, displayPosition, sound = `img/train/${cId}/departure.mp3`, photo = `img/train/${cId}/photo.png`, src = `img/train/${cId}/diagram.png`) {
         this.cId = cId;
         this.width = width;
         this.height = height;
+        this.stationPosition = stationPosition;
+        this.displayPosition = displayPosition;
         this.src = src;
+        this.photo = photo;
+        this.sound = sound;
         this.title = title;
         this.startService = startService;
         this.endService = endService;
@@ -14,25 +18,30 @@ class Train {
     }
 }
 
-const trains = [new Train(54, 1825, 1857, "Locomotion No. 1"), new Train(1904, 1904, 1964, "1904 L&Y Railway EMUs"), new Train(502, 1939, 1980), new Train(507, 1978, 2024), new Train(777, 2024, "Present")]
+const trains = [new Train(54, 1825, 1857, "Locomotion No. 1", "20vw", "24vh", "10vw", "10vw"), new Train(1904, 1904, 1964, "1904 L&Y Railway EMUs"), new Train(502, 1939, 1980), new Train(507, 1978, 2024), new Train(777, 2024, "Present")]
 
-let currentClass = trains.length - 1;
+let currentClass = 0;
 let queuedClass = currentClass;
+
+function getCurrentTrain() {
+    return trains[currentClass];
+}
 
 function setTrainState(train, className) {
     train.className = className;
-    console.log(className)
 }
 
 function updateTrainInfo(train) {
     titleElement.textContent = train.title;
     yearsElement.textContent = train.getServiceYears();
-    photoElement.src = train.src;
+    photoElement.src = train.photo;
 }
 
 function setClass(element, train) {
     element.src = train.src;
     element.alt = train.title;
+    element.style.width = train.width;
+    element.style.height = train.height;
     currentClass = trains.indexOf(train);
 
     updateTrainInfo(train);
@@ -51,6 +60,12 @@ function previousClass() {
 function toLeft() {
     state = 'toLeft';
     setTrainState(element, 'left');
+
+    element.style.left = '';
+    let audio = new Audio(trains[currentClass].sound);
+    audio.play().catch(error => {
+        console.error("Audio playback failed:", error);
+    });
 }
 
 // Reset train to the right side
@@ -67,11 +82,25 @@ function toRight() {
 function toStation() {
     state = 'toStation';
     setTrainState(element, 'station');
+
+    let train = getCurrentTrain();
+    if (train.stationPosition) {
+        element.style.left = train.stationPosition;
+    } else {
+        element.style.left = '';
+    }
 }
 
 function toDisplay() {
     state = 'toDisplay';
     setTrainState(element, "center");
+
+    let train = getCurrentTrain();
+    if (train.displayPosition) {
+        element.style.left = train.displayPosition;
+    } else {
+        element.style.left = '';
+    }
 }
 
 const element = document.getElementById('train');
